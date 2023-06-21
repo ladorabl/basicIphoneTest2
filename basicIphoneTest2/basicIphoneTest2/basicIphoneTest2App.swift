fe541b28964d1e98a7b1c3c629ec5360d580b6db
Larijohn Adorable <larijohn@appdome.com>
2023-06-20 18:10:50 -0700

    init commit

---
+++ basicIphoneTest2/basicIphoneTest2/basicIphoneTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  basicIphoneTest2App.swift
+//  basicIphoneTest2
+//
+//  Created by Larijohn Adorable on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct basicIphoneTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
